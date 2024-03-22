b9f4cab3ff020fef3b379d4ceae1809aae8cb277
Tanvir Anjum Labir <33982784+Labir852@users.noreply.github.com>
2024-03-22 19:37:57 +0600

    created view

---
+++ Controllers/Menu.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MenuFreeCodeCamp.Data;
+using MenuFreeCodeCamp.Models;
+using Microsoft.EntityFrameworkCore;
+namespace MenuFreeCodeCamp.Controllers
+{
+    public class Menu : Controller
+    {
+        private readonly MenuContext _context;
+        public Menu(MenuContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Dishes.ToListAsync());
+        }
+    }
+}
